b26b61933ef1d3384a97aa7935001a1754239f4b
Lewin Kelly <heli@odex.cf>
2024-02-13 14:51:21 +0000

    Force length as 20 due to lack of results

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 	import Wordlist from "$lib/words.json"
 	import Debrief from "$lib/Debrief.svelte"
 
-	const length = Math.random() > 0.5 ? 10 : 20
+	const length = 20
 	let randomWords: string[] = []
 	let recalledWords: string[] = $state(Array(length).fill(""))
 
