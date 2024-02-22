ca99ed3e2c19b015edb504a5999eacea700f6795
Lewin Kelly <heli@odex.cf>
2024-02-22 04:41:04 +0000

    Conclude experiment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,53 +1,31 @@
-<script lang="ts">
-	import Info from "$lib/Info.svelte"
-	import Consent from "$lib/Consent.svelte"
-	import Countdown from "$lib/Countdown.svelte"
-	import Words from "$lib/Words.svelte"
-	import Recall from "$lib/Recall.svelte"
-	import Results from "$lib/Results.svelte"
-
-	import Wordlist from "$lib/words.json"
-	import Debrief from "$lib/Debrief.svelte"
-
-	const length = 20
-	let randomWords: string[] = []
-	let recalledWords: string[] = $state(Array(length).fill(""))
-
-	for (let i = 0; i < length; i++) {
-		// prevent duplicates
-		const randomWord = Wordlist[Math.floor(Math.random() * Wordlist.length)]
-		if (!randomWords.includes(randomWord)) randomWords.push(randomWord)
-		else i--
-	}
-	
-	let page = $state(0)
-
-	$effect(() => {
-		if (localStorage.getItem("done") === "true") page = 7
-		else if (localStorage.getItem("sent") === "true") page = 6
-		else page = 1
-	})
-
-	const go = () => page++
-</script>
-
-{#if page == 1}
-	<Consent {go} />
-{:else if page == 2}
-	<Info {go} {length}/>
-{:else if page == 3}
-	<Countdown {go} />
-{:else if page == 4}
-	<Words {go} words={randomWords} />
-{:else if page == 5}
-	<Recall {length} recall={(list: string[]) => {
-		recalledWords = list
-		go()
-	}}/>
-{:else if page == 6}
-	<Results {go} words={randomWords} recalled={recalledWords} />
-{:else if page == 7}
-	<Debrief  {length}/>	
-{:else}
-	<h1>Loading...</h1>
-{/if}
+<div class="max-w-screen px-2 w-200">
+	<h1 class="text-2xl">Thank you!</h1>
+	<p>This experiment has now concluded. 25 responses were obtained.</p>
+	<p>
+		The purpose of this study was to investigate the <a
+			href="https://wiki2.org/en/Serial-position_effect"
+			target="_blank">
+			serial-position effect.
+		</a>
+		This is the tendency for people to remember the items near the start or end
+		of a list better than the middle items. The data from the experiment will
+		be used in my Higher Psychology assignment.
+	</p>
+	<p>
+		If you were a participant in this study and wish to withdraw from the
+		study, you may email me at <a
+			href="mailto:heli@odex.cf"
+			target="_blank">
+			heli@odex.cf.
+		</a>
+		If you have any questions or concerns, don't hesitate to ask.
+	</p>
+	<p>
+		The source code for this website can be found at <a
+			href="https://github.com/Heliodex/Serial-position-effect"
+			target="_blank">
+			github.com/Heliodex/Serial-position-effect.
+		</a>
+	</p>
+	<p>Thank you for participating in this study!</p>
+</div>
